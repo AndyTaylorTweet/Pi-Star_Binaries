7e086823d7190a8842273ef4ec01db0ccf5bfa68
Andy Taylor <andy@always-online.co.uk>
2017-08-18 09:39:41 +0100

    Update platformDetect.sh

--- platformDetect.sh
+++ platformDetect.sh
@@ -14,34 +14,34 @@ then
 
 	# Make the board revision human readable
 	case $boardRev in
-	*0002) raspberryVer="Model B Revision 1.0 (256MB)";;
-	*0003) raspberryVer="Model B Revision 1.0 + ECN0001 (no fuses, D14 removed) (256MB)";;
-	*0004) raspberryVer="Model B Revision 2.0 Mounting holes (256MB)";;
-	*0005) raspberryVer="Model B Revision 2.0 Mounting holes (256MB)";;
-	*0006) raspberryVer="Model B Revision 2.0 Mounting holes (256MB)";;
+	*0002) raspberryVer="Model B Rev 1.0 (256MB)";;
+	*0003) raspberryVer="Model B Rev 1.0 + ECN0001 (no fuses, D14 removed) (256MB)";;
+	*0004) raspberryVer="Model B Rev 2.0 (256MB)";;
+	*0005) raspberryVer="Model B Rev 2.0 (256MB)";;
+	*0006) raspberryVer="Model B Rev 2.0 (256MB)";;
 	*0007) raspberryVer="Model A Mounting holes (256MB)";;
 	*0008) raspberryVer="Model A Mounting holes (256MB)";;
 	*0009) raspberryVer="Model A Mounting holes (256MB)";;
-	*000d) raspberryVer="Model B Revision 2.0 Mounting holes (512MB)";;
-	*000e) raspberryVer="Model B Revision 2.0 Mounting holes (512MB)";;
-	*000f) raspberryVer="Model B Revision 2.0 Mounting holes (512MB)";;
-	*0010) raspberryVer="Model B+ Revision 1.0 (512MB)";;
-	*0011) raspberryVer="Compute Module 1 Revision 1.0 (512MB)";;
-	*0012) raspberryVer="Model A+ Revision 1.1 (256MB)";;
-	*0013) raspberryVer="Model B+ Revision 1.2 (512MB)";;
-	*0014) raspberryVer="Compute Module 1 Revision 1.0 (512MB)";;
-	*0015) raspberryVer="Model A+ Revision 1.1";;
+	*000d) raspberryVer="Model B Rev 2.0 (512MB)";;
+	*000e) raspberryVer="Model B Rev 2.0 (512MB)";;
+	*000f) raspberryVer="Model B Rev 2.0 (512MB)";;
+	*0010) raspberryVer="Model B+ Rev 1.0 (512MB)";;
+	*0011) raspberryVer="Compute Module 1 Rev 1.0 (512MB)";;
+	*0012) raspberryVer="Model A+ Rev 1.1 (256MB)";;
+	*0013) raspberryVer="Model B+ Rev 1.2 (512MB)";;
+	*0014) raspberryVer="Compute Module 1 Rev 1.0 (512MB)";;
+	*0015) raspberryVer="Model A+ Rev 1.1";;
 	*a01040) raspberryVer="Pi 2 Model B (1GB) - Sony, UK";;
 	*a01041) raspberryVer="Pi 2 Model B (1GB) - Sony, UK";;
-	*a21041) raspberryVer="Pi 2 Model B (1GB) - Embest, China";;
-	*900021) raspberryVer="Model A+ Revision 1.1 (512MB)";;
-	*900032) raspberryVer="Model B+ Revision 1.2 (512MB)";;
+	*a21041) raspberryVer="Pi 2 Model B (1GB) - Embest, CH";;
+	*900021) raspberryVer="Model A+ Rev 1.1 (512MB)";;
+	*900032) raspberryVer="Model B+ Rev 1.2 (512MB)";;
 	*900092) raspberryVer="Pi Zero Rev 1.2 (512MB)";;
 	*900093) raspberryVer="Pi Zero Rev 1.3 (512MB)";;
 	*920093) raspberryVer="Pi Zero Rev 1.3 (512MB)";;
 	*9000c1) raspberryVer="Pi Zero W Rev 1.1 (512MB)";;
 	*a02082) raspberryVer="Pi 3 Model B (1GB) - Sony, UK";;
-	*a22082) raspberryVer="Pi 3 Model B (1GB) - Embest, China";;
+	*a22082) raspberryVer="Pi 3 Model B (1GB) - Embest, CH";;
 	*a32082) raspberryVer="Pi 3 Model B (1GB) - Sony, UK";;
 	*) raspberryVer="Unknown ARM based System";;
 	esac
